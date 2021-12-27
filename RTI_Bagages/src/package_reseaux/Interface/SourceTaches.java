6a023e94beacf17617e427332e3bcc608cd23d13
Lionel-Driessens <78431720+Lionel-Driessens@users.noreply.github.com>
2021-12-27 13:42:17 +0100

    Add files via upload

---
+++ RTI_Bagages/src/package_reseaux/Interface/SourceTaches.java
@@ -0,0 +1,7 @@
+package package_reseaux.Interface;
+
+public interface SourceTaches {
+    public Runnable getTache() throws InterruptedException;
+    public  boolean existTaches();
+    public void recordTache (Runnable r);
+}
